0eb6bf0bfdebc1b652ef622ed964223be5e971bd
iMadfxq <imadfxq@gmail.com>
2022-11-05 23:36:47 -0400

    Initializing firebase, create signup form

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYQCEYKIPDEZm_v40cnKOOSjEU6RvwPhI",
+  authDomain: "financetracker-82ac3.firebaseapp.com",
+  projectId: "financetracker-82ac3",
+  storageBucket: "financetracker-82ac3.appspot.com",
+  messagingSenderId: "702419963858",
+  appId: "1:702419963858:web:d775097e3fb32cf5b9020b"
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig)
+
+
+//init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+
+export {projectFirestore, projectAuth}
--- src/routes/Signup/Signup.component.jsx
+++ src/routes/Signup/Signup.component.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react"
+
 export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Signup</div>
+    <section>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>email</span>
+          <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
+        </label>
+        <label>
+          <span>password</span>
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+        </label>
+        <label>
+          <span>username</span>
+          <input type="text" value={username} onChange={e => setUsername(e.target.value)}/>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
   )
 }
