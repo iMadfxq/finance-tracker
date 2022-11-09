3d2104b8df97f06de9d4106de332657597cea883
iMadfxq <imadfxq@gmail.com>
2022-11-08 19:20:35 -0500

    Finished logic to add documents to firebase

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,27 +1,75 @@
-import { useReducer, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { projectFirestore } from "../firebase/config";
 
 let intialState = {
-  document:null,
+  document: null,
   isPending: false,
   error: null,
-  success:null
-}
+  success: null,
+};
 
 const firestoreReducer = (state, action) => {
   switch (action.type) {
+    case "IS_PENDING":
+      return {
+        ...state,
+        isPending: true,
+        error: null,
+        success: false,
+        document: null,
+      };
+    case "ADDED_DOCUMENT":
+      return {
+        ...state,
+        document: action.payload,
+        isPending: false,
+        success: true,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        ...state,
+        isPending: false,
+        success: false,
+        document: null,
+        error: action.payload,
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
-export const useFirestore = (collection)=> {
-  const [response, dispatch] = useReducer(firestoreReducer, intialState)
-  const [isCancelled, setIsCancelled] = useState(false)
+export const useFirestore = (collection) => {
+  const [response, dispatch] = useReducer(firestoreReducer, intialState);
+  const [isCancelled, setIsCancelled] = useState(false);
+
+  const dispatchIfNotCanceled = (action) => {
+    if (!isCancelled) {
+      dispatch(action);
+    }
+  };
 
   //collection reference
-  const ref = projectFirestore.collection(collection)
+  const ref = projectFirestore.collection(collection);
 
-  
+  //add documents to collection
+  const addDocument = async (doc) => {
+    dispatch({ type: "IS_PENDING" });
 
-}
+    try {
+      const addedDocument = await ref.add(doc);
+      dispatchIfNotCanceled({ type: "ADDED_DOCUMENT", payload: addedDocument });
+    } catch (err) {
+      dispatchIfNotCanceled({ type: "ERROR", payload: err });
+    }
+  };
+
+  //delete documents
+  const deleteDocument = async (id) => {};
+
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { addDocument, deleteDocument, response };
+};
